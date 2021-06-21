08e485159d058538add3d7326676cc07b044c43a
Leonardo Souza <leonardo030605@gmail.com>
2021-06-21 10:27:30 -0300

    Html e CSS da pagina inicial e de equipes pronto

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function botaoDoMenu(){
+    var menu = document.getElementById("links");
+    if(menu.style.display === "flex"){
+        // Esconde o Menu
+        menu.style.display = "none";
+    } else{
+        // Mostre o Menu
+        menu.style.display = "flex";
+    }
+}
